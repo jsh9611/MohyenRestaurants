fc20f2e93c11cde77932e9b7cc4b5b993bd12421
jsh9611 <jangsh9611@naver.com>
2021-12-04 17:01:15 +0900

    [update] map.html에 db에서 불러온 데이터를 변환해서 전달하도록함

--- map/views.py
+++ map/views.py
@@ -1,5 +1,56 @@
 from django.shortcuts import render
+from .models import Restaurants
+import json
 
-# Create your views here.
 def showmap(request):
-    return render(request, 'map/map.html')
+    qs = Restaurants.objects.all() # 테이블의 모든 row를 할당
+    # ret_dict = {}
+    # i = 0
+    # map_dic = {}
+    # orderList: Restaurants = qs.values()
+    
+    # for i in range(len(qs)):
+    #     'id': qs[i]['id'],
+    #     'name': 
+    #     'kinds': 
+    #     'score': 
+    #     'review':  
+    #     'web_link': 
+    #     'address':  
+    #     'lat': 
+    #     'lng': 
+    #     'category':
+    # print("aaaaaaaaaaaaaaaaaaaa")
+    # print(qs.values())
+    # print("bbbbbbbaabbbb")
+    # print(qs.values()[0])
+    # print(len(qs))
+    rows = []
+    for i in range(len(qs)):
+        rows.append(qs.values()[i])
+    print(rows)
+
+    map_dict = {}
+    
+    for i in range(len(qs)):
+        map_dict['item'+str(i)] = ( json.dumps(qs.values()[i], ensure_ascii=False) )
+    print(33333333333333)
+    print(map_dict)
+
+    # rows = (qs)
+    context = {'rows': map_dict}
+    print(context)
+    return render(request, 'map/map.html', context)
+"""
+    for item in rst_data:
+        'id':  
+        'name': 
+        'kinds': 
+        'score': 
+        'review':  
+        'web_link': 
+        'address':  
+        'lat': 
+        'lng': 
+        'category': 
+"""
