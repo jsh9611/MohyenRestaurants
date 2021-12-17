af0515531289cbd90b4a700a98262d8840c918d3
jsh9611 <jangsh9611@naver.com>
2021-12-17 18:43:05 +0900

    지도 models 주석 변경

--- map/models.py
+++ map/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 class Restaurants(models.Model):
     id = models.AutoField(primary_key=True)     # id
-    name = models.CharField(max_length=20)      # 구 이름
-    kinds = models.CharField(max_length=20)     # 도로명
+    name = models.CharField(max_length=20)      # 가게명
+    kinds = models.CharField(max_length=20)     # 업종분야
     score = models.FloatField()                 # 별점
     review = models.CharField(max_length=20)    # 리뷰 수
     web_link = models.CharField(max_length=200) # 웹페이지 주소
